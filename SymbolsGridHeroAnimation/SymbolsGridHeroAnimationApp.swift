4ac1e89aeca4c1bc4fa7245379eebfecbf6deb97
Ed <Siniakov>
2023-08-25 13:28:10 +0300

    Initial Commit

---
+++ SymbolsGridHeroAnimation/SymbolsGridHeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolsGridHeroAnimationApp.swift
+//  SymbolsGridHeroAnimation
+//
+//  Created by Eduard Kanevskii on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SymbolsGridHeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
